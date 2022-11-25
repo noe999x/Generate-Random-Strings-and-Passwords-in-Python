33bc4a6d52831150b8c3a7637615870e8bedd14c
ikz <68614343+noe999x@users.noreply.github.com>
2022-11-26 02:18:48 +0700

    Update token.py

--- token.py
+++ token.py
@@ -18,7 +18,7 @@ clearall()
 token=secrets.token_hex(12) #module import from secrets
 username=input(' • Username : ') #for input user username
 noe('Welcome',username);time.sleep(1) #welcome to user login
-noe('\nYour token :',token) #output from uuid
+noe('\nYour token :',token) #output from secrets module
 password=input(' • Token : ') #for input user apikey
 anim('Please wait...') #from oading to your destiny
 if password==str(token):
@@ -28,5 +28,5 @@ if password==str(token):
 else:
      password!=str(token)
      time.sleep(2)
-     noe('\n[red]Login failed, check again your apikey[reset]')
-     time.sleep(2); #failed or wrong apikey
+     noe('\n[red]Login failed, check again your token[reset]')
+     time.sleep(2); #failed or wrong token
